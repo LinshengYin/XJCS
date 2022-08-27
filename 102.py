964c0b1cabebc1372c0f1e7d825c2d9904ecbf63
Linsheng Yin <93458141+LinshengYin@users.noreply.github.com>
2022-08-27 16:18:25 +0800

    Delete 102.py

--- 102.py
+++
@@ -1,9 +0,0 @@
-data=input()
-list=[]
-for i in range(len(data)):
-    for j in range(i,len(data)):
-        list.append(data[i:j+1])
-for q in list:
-    print(q,end=' ')
-print()
-print(len(list)+1)
